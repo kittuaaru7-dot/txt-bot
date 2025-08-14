f0332c3c179dc346469e64a0476ad72d5076ff74
kittuaaru7-dot <kittuaaru7@gmail.com>
2025-08-15 03:01:00 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,15 +1,4 @@
-#KITTU
-# Add your details here and then deploy by clicking on HEROKU Deploy button
-import os
-from os import environ
+3#KITTU
 
-API_ID = int(environ.get("API_ID", "21113148"))
-API_HASH = environ.get("API_HASH", "908feafcf7973178ac490b8c35c087d9")
-BOT_TOKEN = environ.get("BOT_TOKEN",7948116587:AAEr904PSHeFyq9qBvwMiKOkvQc2FH_OH5A "")
-OWNER = int(environ.get("OWNER", "6658266490"))
-CREDIT = environ.get("CREDIT", "KITTU BOT")
-LOG_CHANNEL = "-1002772799692# log group me bot ko admin narur banaye full rights ke sath
-#WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
 
 
